9d1797d196ca99ecdca13b57dec6f30d69ff69a5
Thomas Kavanagh <tom.revsw@gmail.com>
2014-08-17 19:57:55 -0700

    Initial version of revsw_mod_install.sh
    Script to install the various RevSw modules on a server
    when a new version is released.  Will be packaged as
    part of the release tarball.
    
    Signed-off-by: Thomas Kavanagh <tom.revsw@gmail.com>

---
+++ revsw_mod_install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+kern=$(uname -r)
+
+echo $kern
+
+# Ensure the extra directory exists for the
+# currently running kernel
+if [ ! -d "/lib/modules/$kern/extra" ]; then
+	mkdir /lib/modules/$kern/extra
+fi
+
+cp revsw/*.ko /lib/modules/$kern/extra
+
+depmod -a
+
+cp 10-enable-revsw-tcp-module.conf /etc/sysctl.d/.
+
+sysctl -p /etc/sysctl.d/10-enable-revsw-tcp-module.conf
+
