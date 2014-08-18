ee4ea8edaa70168e793d434227e1af44ae90eabb
Thomas Kavanagh <tom.revsw@gmail.com>
2014-08-17 19:59:59 -0700

    Add Revsw module installation commands to build-kernel-pkg.sh
    Updated the build-kernel-pkg.sh script to create a release tarball
    that contains the following:
    - linux-image
    - all RevSw modules
    - RevSw sysctl config file
    - RevSw module install script
    
    Signed-off-by: Thomas Kavanagh <tom.revsw@gmail.com>

--- build-kernel-pkg.sh
+++ build-kernel-pkg.sh
@@ -15,3 +15,23 @@ cd ../revsw
 
 make ARCH=x86_64 -C ../linux M=$PWD
 
+cd ..
+
+# Now package everything together in a tar file
+
+imageFile=(linux-image-*.deb)
+modFiles=$(ls revsw/*.ko)
+
+if [ ! -f "$imageFile" ]; then
+        echo "Linux image file does NOT exist"
+        return
+fi
+
+version=${imageFile:12:17}
+
+# Ensure the install script is executable
+chmod +x revsw_mod_install.sh
+
+tar cvf Revsw-linux-$version-amd64.tar $imageFile $modFiles revsw_mod_install.sh 10-enable-revsw-tcp-module.conf
+
+rm *.deb
