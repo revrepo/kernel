f315118039d67ea05a34350c6a45c3c013f23cbe
Tom Kavanagh <tom.revsw@gmail.com>
2014-10-16 14:31:26 -0700

    DOTS-35: Add tcp_revsw_version.h file
    This file is used to modify the major, minor and submodel version
    numbers of the Rev Software module.
    
    Signed-off-by: Tom Kavanagh <tom.revsw@gmail.com>

---
+++ revsw/tcp_revsw_version.h
@@ -0,0 +1,17 @@
+/*
+ *
+ *   RevSw TCP Version file
+ *
+ * Copyright (c) 2013-2014, Rev Software, Inc.
+ * All Rights Reserved.
+ * This code is confidential and proprietary to Rev Software, Inc
+ * and may only be used under a license from Rev Software Inc.
+ */
+#ifndef __TCP_REVSW_VERSION_H__
+#define __TCP_REVSW_VERSION_H__
+
+#define TCP_REVSW_MAJOR		1
+#define TCP_REVSW_MINOR		0
+#define TCP_REVSW_SUBLEVEL	0
+
+#endif /* __TCP_REVSW_VERSION_H__ */
